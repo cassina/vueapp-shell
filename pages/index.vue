aedf147c9c3bf20c84efaa72830a242c0f7945e8
cassina <bernardocassina@gmail.com>
2022-01-08 13:07:57 -0600

    Add files XD.

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <navigation />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Navigation from "~/components/Navigation.vue";
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {Navigation},
+})
+</script>
